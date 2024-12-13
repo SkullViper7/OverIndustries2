88f64ef880bb7ac5a3f935792f3ef847dba41278
Eva291204 <eva.fricaud@e-artsup.net>
2024-12-13 11:43:23 +0100

    mise en place animation employés

---
+++ Assets/Scripts/Employees/InteractAnimation.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractAnimation : MonoBehaviour
+{
+    [field : SerializeField] public List<string> _interactTriggerAnimation { get; private set; }
+}
