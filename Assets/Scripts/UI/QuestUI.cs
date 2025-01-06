602cae7b91eddd129f5a488a2dbf560d1ff67660
Eva291204 <eva.fricaud@e-artsup.net>
2025-01-06 14:21:08 +0100

    update ui quest

--- Assets/Scripts/UI/QuestUI.cs
+++ Assets/Scripts/UI/QuestUI.cs
@@ -34,7 +34,8 @@ public class QuestUI : MonoBehaviour
         }
 
         QuestManager.Instance.NewQuestGenerate += ShowQuestInfo;
-        QuestManager.Instance.UpdateAdvancementQuest += UpdateCurrentAdvancementQuest;
+        QuestManager.Instance.UpdateAdvancementQuest += UpdateCurrentAdvancementQuestComponent;
+        QuestManager.Instance.UpdateAdvancementQuest += UpdateCurrentAdvancementQuestObject;
         QuestManager.Instance.QuestComplited += QuestComplited;
         QuestManager.Instance.ResetQuestText += ResetQuestText;
     }
@@ -58,19 +59,29 @@ public class QuestUI : MonoBehaviour
             }
             if (_quest.QuestData.NumberOfComponent.Count == 2)
             {
-                QuestObjectifText.text = $"{_quest.QuestData.NumberOfComponent[0]} {_quest.QuestData.Component[0].Name} \n{_quest.QuestData.NumberOfComponent[1]} {_quest.QuestData.Component[1].Name}";
+                QuestObjectifText.text = $"{_quest.QuestData.NumberOfComponent[0]} {_quest.QuestData.Component[0].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[1]} {_quest.QuestData.Component[1].Name}";
             }
             if (_quest.QuestData.NumberOfComponent.Count == 3)
             {
-                QuestObjectifText.text = $"{_quest.QuestData.NumberOfComponent[0]} {_quest.QuestData.Component[0].Name} \n{_quest.QuestData.NumberOfComponent[1]} {_quest.QuestData.Component[1].Name} \n{_quest.QuestData.NumberOfComponent[2]} {_quest.QuestData.Component[2].Name}";
+                QuestObjectifText.text = $"{_quest.QuestData.NumberOfComponent[0]} {_quest.QuestData.Component[0].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[1]} {_quest.QuestData.Component[1].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[2]} {_quest.QuestData.Component[2].Name}";
             }
             if (_quest.QuestData.NumberOfComponent.Count == 4)
             {
-                QuestObjectifText.text = $"{_quest.QuestData.NumberOfComponent[0]} {_quest.QuestData.Component[0].Name} \n{_quest.QuestData.NumberOfComponent[1]} {_quest.QuestData.Component[1].Name} \n{_quest.QuestData.NumberOfComponent[2]} {_quest.QuestData.Component[2].Name} \n{_quest.QuestData.NumberOfComponent[3]} {_quest.QuestData.Component[3].Name}";
+                QuestObjectifText.text = $"{_quest.QuestData.NumberOfComponent[0]} {_quest.QuestData.Component[0].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[1]} {_quest.QuestData.Component[1].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[2]} {_quest.QuestData.Component[2].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[3]} {_quest.QuestData.Component[3].Name}";
             }
             if (_quest.QuestData.NumberOfComponent.Count == 5)
             {
-                QuestObjectifText.text = $"{_quest.QuestData.NumberOfComponent[0]} {_quest.QuestData.Component[0].Name} \n{_quest.QuestData.NumberOfComponent[1]} {_quest.QuestData.Component[1].Name} \n{_quest.QuestData.NumberOfComponent[2]} {_quest.QuestData.Component[2].Name} \n{_quest.QuestData.NumberOfComponent[3]} {_quest.QuestData.Component[3].Name} \n{_quest.QuestData.NumberOfComponent[4]} {_quest.QuestData.Component[4].Name}";
+                QuestObjectifText.text = $"{_quest.QuestData.NumberOfComponent[0]} {_quest.QuestData.Component[0].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[1]} {_quest.QuestData.Component[1].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[2]} {_quest.QuestData.Component[2].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[3]} {_quest.QuestData.Component[3].Name} " +
+                    $"\n{_quest.QuestData.NumberOfComponent[4]} {_quest.QuestData.Component[4].Name}";
             }
         }
 
@@ -82,11 +93,16 @@ public class QuestUI : MonoBehaviour
             }
             if (_quest.QuestData.NumberOfObject.Count == 2)
             {
-                QuestObjectifText.text = $"{QuestObjectifText.text} {_quest.QuestData.NumberOfObject[0]} {_quest.QuestData.Objects[0].Name} \n{_quest.QuestData.NumberOfObject[1]} {_quest.QuestData.Objects[1].Name}";
+                QuestObjectifText.text = $"{QuestObjectifText.text} " +
+                    $"{_quest.QuestData.NumberOfObject[0]} {_quest.QuestData.Objects[0].Name} " +
+                    $"\n{_quest.QuestData.NumberOfObject[1]} {_quest.QuestData.Objects[1].Name}";
             }
             if (_quest.QuestData.NumberOfObject.Count == 3)
             {
-                QuestObjectifText.text = $"{QuestObjectifText.text} {_quest.QuestData.NumberOfObject[0]} {_quest.QuestData.Objects[0].Name} \n{_quest.QuestData.NumberOfObject[1]} {_quest.QuestData.Objects[1].Name} \n{_quest.QuestData.NumberOfObject[2]} {_quest.QuestData.Objects[2].Name}";
+                QuestObjectifText.text = $"{QuestObjectifText.text} " +
+                    $"{_quest.QuestData.NumberOfObject[0]} {_quest.QuestData.Objects[0].Name} " +
+                    $"\n{_quest.QuestData.NumberOfObject[1]} {_quest.QuestData.Objects[1].Name} " +
+                    $"\n{_quest.QuestData.NumberOfObject[2]} {_quest.QuestData.Objects[2].Name}";
             }
         }
     }
@@ -95,7 +111,8 @@ public class QuestUI : MonoBehaviour
     {
         QuestManager.Instance.NewCurrentQuest(_actualQuest);
 
-        UpdateCurrentAdvancementQuest();
+        UpdateCurrentAdvancementQuestObject();
+        UpdateCurrentAdvancementQuestComponent();
     }
     public void QuestRefuse()
     {
@@ -103,9 +120,72 @@ public class QuestUI : MonoBehaviour
     }
 
     /// <summary>
+    /// Update the actual quest and there avancement with the actual storage of this component type
+    /// </summary>
+    public void UpdateCurrentAdvancementQuestComponent()
+    {
+        for (int i = 0; i < QuestManager.Instance.CurrentQuestList.Count; i++)
+        {
+            Quest _currentQuest = QuestManager.Instance.CurrentQuestList[i];
+            _nameCurrentQuestTextList[i].text = _currentQuest.QuestData.Name;
+
+            if (_currentQuest.QuestData.NumberOfComponent.Count == 1)
+            {
+                _objectifCurrentQuestTextList[i].text = 
+                    $"{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[0])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[0]}{_currentQuest.QuestData.Component[0].Name}";
+            }
+            if (_currentQuest.QuestData.NumberOfComponent.Count == 2)
+            {
+                _objectifCurrentQuestTextList[i].text = 
+                    $"{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[0])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[0]} {_currentQuest.QuestData.Component[0].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[1])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[1]} {_currentQuest.QuestData.Component[1].Name}";
+            }
+            if (_currentQuest.QuestData.NumberOfComponent.Count == 3)
+            {
+                _objectifCurrentQuestTextList[i].text = 
+                    $"{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[0])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[0]} {_currentQuest.QuestData.Component[0].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[1])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[1]} {_currentQuest.QuestData.Component[1].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[2])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[2]} {_currentQuest.QuestData.Component[2].Name}";
+            }
+            if (_currentQuest.QuestData.NumberOfComponent.Count == 4)
+            {
+                _objectifCurrentQuestTextList[i].text = 
+                    $"{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[0])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[0]} {_currentQuest.QuestData.Component[0].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[1])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[1]} {_currentQuest.QuestData.Component[1].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[2])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[2]} {_currentQuest.QuestData.Component[2].Name}" +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[3])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[3]} {_currentQuest.QuestData.Component[3].Name}";
+            }
+            if (_currentQuest.QuestData.NumberOfComponent.Count == 5)
+            {
+                _objectifCurrentQuestTextList[i].text = 
+                    $"{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[0])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[0]} {_currentQuest.QuestData.Component[0].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[1])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[1]} {_currentQuest.QuestData.Component[1].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[2])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[2]} {_currentQuest.QuestData.Component[2].Name}" +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[3])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[3]} {_currentQuest.QuestData.Component[3].Name}"+
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisComponent(_currentQuest.QuestData.Component[4])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfComponent[4]} {_currentQuest.QuestData.Component[4].Name}";
+            }
+        }
+    }
+
+    /// <summary>
     /// Update the actual quest and there avancement with the actual storage of this object type
     /// </summary>
-    public void UpdateCurrentAdvancementQuest()
+    public void UpdateCurrentAdvancementQuestObject()
     {
         for (int i = 0; i < QuestManager.Instance.CurrentQuestList.Count; i++)
         {
@@ -114,17 +194,27 @@ public class QuestUI : MonoBehaviour
 
             if (_currentQuest.QuestData.NumberOfObject.Count == 1)
             {
-                _objectifCurrentQuestTextList[i].text = $"{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[0])} / {_currentQuest.QuestData.NumberOfObject[0]} {_currentQuest.QuestData.Objects[0].Name}";
+                _objectifCurrentQuestTextList[i].text = 
+                    $"{_objectifCurrentQuestTextList[i].text} {ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[0])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfObject[0]} {_currentQuest.QuestData.Objects[0].Name}";
             }
-
             if (_currentQuest.QuestData.NumberOfObject.Count == 2)
             {
-                _objectifCurrentQuestTextList[i].text = $"{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[0])} / {_currentQuest.QuestData.NumberOfObject[0]} {_currentQuest.QuestData.Objects[0].Name} \n{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[1])} / {_currentQuest.QuestData.NumberOfObject[1]} {_currentQuest.QuestData.Objects[1].Name}";
+                _objectifCurrentQuestTextList[i].text = 
+                    $"{_objectifCurrentQuestTextList[i].text} {ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[0])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfObject[0]} {_currentQuest.QuestData.Objects[0].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[1])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfObject[1]} {_currentQuest.QuestData.Objects[1].Name}";
             }
-
             if (_currentQuest.QuestData.NumberOfObject.Count == 3)
             {
-                _objectifCurrentQuestTextList[i].text = $"{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[0])} / {_currentQuest.QuestData.NumberOfObject[0]} {_currentQuest.QuestData.Objects[0].Name} \n{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[1])} / {_currentQuest.QuestData.NumberOfObject[1]} {_currentQuest.QuestData.Objects[1].Name} \n{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[2])} / {_currentQuest.QuestData.NumberOfObject[2]} {_currentQuest.QuestData.Objects[2].Name}";
+                _objectifCurrentQuestTextList[i].text = 
+                    $"{_objectifCurrentQuestTextList[i].text} {ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[0])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfObject[0]} {_currentQuest.QuestData.Objects[0].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[1])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfObject[1]} {_currentQuest.QuestData.Objects[1].Name} " +
+                    $"\n{ItemStorage.Instance.ReturnNumberOfThisObject(_currentQuest.QuestData.Objects[2])} " +
+                    $"/ {_currentQuest.QuestData.NumberOfObject[2]} {_currentQuest.QuestData.Objects[2].Name}";
             }
         }
     }
@@ -140,8 +230,8 @@ public class QuestUI : MonoBehaviour
             _objectifCurrentQuestTextList[i].color = Color.black;
 
             _buttonQuestComplitedList[i].SetActive(false);
-            _nameCurrentQuestTextList[i].text = " ";
-            _objectifCurrentQuestTextList[i].text = " ";
+            _nameCurrentQuestTextList[i].text = "";
+            _objectifCurrentQuestTextList[i].text = "";
         }
     }
 
