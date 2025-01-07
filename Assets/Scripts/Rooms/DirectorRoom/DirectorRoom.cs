dc7e82c48345184c0736e036650e8005872d779d
Eva291204 <eva.fricaud@e-artsup.net>
2025-01-07 15:29:59 +0100

    modif embauche employer

--- Assets/Scripts/Rooms/DirectorRoom/DirectorRoom.cs
+++ Assets/Scripts/Rooms/DirectorRoom/DirectorRoom.cs
@@ -7,26 +7,60 @@ public class DirectorRoom : MonoBehaviour, IRoomBehaviour
     private static DirectorRoom _instance = null;
     public static DirectorRoom Instance => _instance;
 
+    //Reference
     [field: SerializeField] public Room RoomMain { get; private set; }
     [field: SerializeField] public DirectorRoomData DirectorRoomData { get; private set; }
-
     private JobProfileGenerator _jobProfileGenerator;
-    private int _poolEmployeeID;
+
+    //Reference for create employee
+    public int PoolEmployeeID { get; private set; }
     private GameObject _newEmployee;
-    public List<GameObject> RecrutementList { get; private set; } = new List<GameObject>();
+
+    //Wait create new employee
+    private int secondSinceCreateEmployee;
+    [field: SerializeField] public List<GameObject> RecrutementList { get; private set; } = new List<GameObject>();
 
     public event System.Action<Room> MaxAssignEmployee;
+
+    private void Awake()
+    {
+        // Singleton
+        if (_instance != null && _instance != this)
+        {
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
 
     public void InitRoomBehaviour(IRoomBehaviourData roomBehaviourData, Room roomMain)
     {
         DirectorRoomData = (DirectorRoomData)roomBehaviourData;
         RoomMain = roomMain;
     }
+
     public void Start()
     {
         _jobProfileGenerator = JobProfileGenerator.Instance;
-        _poolEmployeeID = ObjectPoolManager.Instance.NewObjectPool(RoomMain.RoomData.Employee);
+        PoolEmployeeID = ObjectPoolManager.Instance.NewObjectPool(DirectorRoomData.Employee);
+        ChronoManager.Instance.NewSecondTick += WaitCreateEmployee;
         CreateEmployee();
+    }
+
+    /// <summary>
+    /// call at new second past
+    /// </summary>
+    public void WaitCreateEmployee()
+    {
+        secondSinceCreateEmployee++;
+
+        if (secondSinceCreateEmployee >= DirectorRoomData.WaitTime)
+        {
+            CreateEmployee();
+            secondSinceCreateEmployee = 0;
+        }
     }
 
     public void CreateEmployee()
@@ -35,16 +69,16 @@ public class DirectorRoom : MonoBehaviour, IRoomBehaviour
         {
             for (int i = 0; RecrutementList.Count < 5; i++)
             {
-                _newEmployee = ObjectPoolManager.Instance.GetObjectInPool(_poolEmployeeID);
-                _newEmployee.SetActive(true);
+                _newEmployee = ObjectPoolManager.Instance.GetObjectInPool(PoolEmployeeID);
                 RecrutementList.Add(_newEmployee);
 
                 _jobProfileGenerator.GenerateProfile(_newEmployee.GetComponent<Employee>());
             }
         }
-        else
-        {
-            MaxAssignEmployee.Invoke(RoomMain);
-        }
+    }
+
+    public void MaxEmployeeAssign()
+    {
+        MaxAssignEmployee.Invoke(RoomMain);
     }
 }
--- Assets/Scripts/Rooms/DirectorRoom/DirectorRoomData.cs
+++ Assets/Scripts/Rooms/DirectorRoom/DirectorRoomData.cs
@@ -20,4 +20,26 @@ public class DirectorRoomData : ScriptableObject, IRoomBehaviourData
     /// </summary> 
     [SerializeField, Tooltip("Capacity to add to the  director room at lvl 3.")]
     private int _capacityBonusAtLvl3;
+    
+    /// <summary>
+    /// Employee prefab for create employee
+    /// </summary>
+    [SerializeField, Tooltip("employee prefab")]
+    private GameObject _employee;
+
+    /// <summary>
+    /// Time to wait before create a new employee
+    /// </summary>
+    [SerializeField, Tooltip("Time to wait before create a new employee")]
+    private int _waitTime;
+    
+    /// <summary>
+    /// Employee prefab for create employee
+    /// </summary>
+    public GameObject Employee { get { return _employee; } }
+
+    /// <summary>
+    /// Time to wait before create a new employee
+    /// </summary>
+    public int WaitTime { get { return _waitTime; } }
 }
